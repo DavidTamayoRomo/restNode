8712b7ba110312813894f7b31943991adaae3859
David <david95tamayo@hotmail.es>
2021-06-27 22:39:55 -0500

    tree commit

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,47 +1,57 @@
-const {response} = require('express')
+const { response } = require('express')
 const Usuario = require('../models/usuario')
 
-const usuariosGet = async (req, res)=> {
+const usuariosGet = async (req, res) => {
     // const query = req.query
-    const usuarios = await Usuario.find();
-    res.json({
-        ok: true,
-        msg:'GET -controlador ',
-        usuarios
-    })
+    try {
+        const usuarios = await Usuario.find();
+        res.status(200).json({
+            ok: true,
+            msg: 'GET -controlador ',
+            usuarios
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Contactese con el administrador ',
+        })
+    }
+
+
 }
 
-const usaurioPost = async (req, res)=> {
+const usaurioPost = async (req, res) => {
     const body = req.body
     const usuario = new Usuario(body)
     await usuario.save();
-    res.json({
+    res.status(200).json({
         ok: true,
-        msg:'POST',
+        msg: 'POST',
         usuario
     })
 }
 
-const usuarioPut = async(req, res)=> {
-    
+const usuarioPut = async (req, res) => {
+
     const id = req.params.id;
-    const body=req.body
-    const usuario = await Usuario.findByIdAndUpdate(id,body)
+    const body = req.body
+    const usuario = await Usuario.findByIdAndUpdate(id, body)
 
-    res.json({
+    res.status(200).json({
         ok: true,
-        msg:'PUT',
+        msg: 'PUT',
         usuario
     })
 }
 
-const usuarioDelete =  async (req, res)=> {
+const usuarioDelete = async (req, res) => {
     const id = req.params.id;
 
     const usuario = await Usuario.findByIdAndDelete(id)
-    res.json({
+    res.status(200).json({
         ok: true,
-        msg:'DELETE',
+        msg: 'DELETE',
         id
     })
 }
